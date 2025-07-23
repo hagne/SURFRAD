2f91853a5bee941414b55f0c412b60fbadeaa004
hagne <hagen.telg@gmail.com>
2025-07-23 10:57:58 +0200

    quick commit

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.1.dev27+g484c346.d20250716'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev27', 'g484c346.d20250716')
+__version__ = version = '0.1.1.dev28+gca045d2.d20250722'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev28', 'gca045d2.d20250722')
